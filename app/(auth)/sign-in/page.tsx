2aae45762a04d55db993aa377d4cc4f8dc751a48
Shubham Singh <shubhamkv1997@gmail.com>
2024-09-25 16:55:27 +0530

    Signing UX

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-import Link from "next/link";
-
 import { Button } from "@/components/ui/button";
-import { Github } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -11,15 +8,24 @@ import {
   signInWithEmailAndPassword,
   signInWithPopup,
 } from "firebase/auth";
-import {
-  Form
-} from "@/components/ui/form";
 import { auth } from "@/configs/Firebase-config";
 import { toast } from "sonner";
 import axios from "axios";
-import { headers } from "next/headers";
 import getHeaders from "@/app/utils/headers.util";
-
+import {
+  arrowcon,
+  chat,
+  cloud,
+  cross,
+  logo60,
+  logoWithText,
+  sendBlue,
+  setting,
+} from "@/public";
+import { LucideCheck, LucideGithub } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
 export default function Signin() {
   const formSchema = z.object({
@@ -97,73 +103,81 @@ export default function Signin() {
       });
   };
   return (
-    <div className="flex items-center justify-center py-12">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="mx-auto grid w-[350px] gap-6"
-        >
-          <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-extrabold">Login</h1>
-            {/* <p className="text-balance text-secondary">
-              Enter your email below to login to your account
-            </span> */}
+    <section className="flex items-center justify-between w-full h-screen relative">
+    <div className="w-full bg-[#515983] h-full flex flex-col items-center justify-center gap-28">
+      <div className="h-3/6 w-3/5 bg-white rounded-3xl relative">
+        <div className="flex justify-between flex-col w-full h-full">
+          <div className="mt-auto py-6 px-4 flex flex-col gap-5 h-full">
+            <div className="flex items-start justify-start gap-2">
+              <div className="w-10 h-10 bg-[#FFF1E0] rounded-full grid place-items-center text-primary">
+                AK
+              </div>
+              <div className="bg-[#FFF1E0] h-20 w-[70%] rounded-lg"></div>
+            </div>
+            <div className="flex items-start justify-end gap-2">
+              <div className="bg-[#E0F3FF] h-20 w-[70%] rounded-lg"></div>
+              <div className="w-10 h-10 bg-[#FFF1E0] rounded-full grid place-items-center text-primary">
+                <Image src={logo60} alt="logo60" className="rounded-full" />
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-2">
+              <div className="w-10 h-10 bg-[#FFF1E0] rounded-full grid place-items-center text-primary">
+                AK
+              </div>
+              <div className="bg-[#FFF1E0] h-10 w-[70%] rounded-lg"></div>
+            </div>
           </div>
-          <div className="grid gap-4">
-            {/* <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem className="grid gap-2">
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="email"
-                      placeholder="m@example.com"
-                      required
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem className="grid gap-2">
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input id="password" type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button
-              type="submit"
-              className="w-full"
-              disabled={form.formState.isSubmitting}
-            >
-              Login
-            </Button> */}
-            <Button
-              type="button"
-              onClick={onGithub}
-              className="w-full text-background bg-[#2b3137] hover:bg-[#2b3137] outline-none border-none hover:opacity-95"
-            >
-              <Github className="mr-2 h-4 w-4 inline " /> Login with Github
-            </Button>
+          <div className="mb-6 w-5/6 mx-auto h-10 flex justify-end border-2 rounded-sm border-[#3E99DB]">
+            <Image src={sendBlue} alt="sendBlue" className="ml-auto mr-2" />
           </div>
-          <div className="mt-4 text-center text-sm">
+        </div>
+        <Image
+          src={arrowcon}
+          className="absolute -bottom-[6rem] right-0 "
+          alt="arrowcon"
+        />
+        <Image src={cross} className="absolute top-0 -right-11" alt="cross" />
+      </div>
+      <div className="text-xl text-center text-white font-bold mb-10">
+        Build AI agents specialised on your <br /> codebase in a minute
+      </div>
+    </div>
+    <div className="w-full h-full flex items-center justify-center flex-col gap-14">
+      <Image src={logoWithText} alt="logo" />
+      <div className="flex items-center justify-center flex-col text-border">
+        {/* <h3 className="text-2xl font-bold text-black">Get Started!</h3> */}
+        {/* <div className="flex items-start justify-start flex-col mt-10 gap-4">
+          <p className="flex items-center justify-center text-start gap-4">
+            <LucideCheck
+              size={20}
+              className="bg-primary rounded-full p-[0.5px] text-white"
+            />
+            Select the repositories you want to build your AI agents on.
+          </p>
+          <p className="flex items-center justify-center text-start gap-4">
+            <LucideCheck
+              size={20}
+              className="bg-primary rounded-full p-[0.5px] text-white"
+            />
+            You can choose to add more repositories later on from the
+            dashboard
+          </p>
+        </div> */}
+        <Button onClick={() => onGithub()} className="mt-14 gap-2 w-80">
+          <LucideGithub className=" rounded-full border border-white p-1" />
+          Signin with GitHub
+        </Button>
+        <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
             <Link href="/sign-up" className="underline">
               Sign up
             </Link>
           </div>
-        </form>
-      </Form>
+      </div>
     </div>
+    <Image src={chat} className="absolute top-0" alt="chat" />
+    <Image src={cloud} className="absolute bottom-2" alt="cloud" />
+    <Image src={setting} className="absolute top-0 right-0" alt="setting" />
+  </section>
   );
 }

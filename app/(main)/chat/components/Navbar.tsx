36090b9b9be2e5fbea380b2ad72af6b3444fa895
Yash Pede <yashpede@ssipmt.com>
2024-10-03 10:52:07 +0530

    feat: share chat

--- app/(main)/chat/components/Navbar.tsx
+++ app/(main)/chat/components/Navbar.tsx
@@ -1,12 +1,10 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import dayjs from "dayjs";
 import {
   Dialog,
   DialogClose,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -14,7 +12,7 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/lib/state/store";
 import { setChat } from "@/lib/state/Reducers/chat";
@@ -23,39 +21,78 @@ import axios from "axios";
 import getHeaders from "@/app/utils/headers.util";
 import { toast } from "sonner";
 import { usePathname } from "next/navigation";
+import { Share2Icon } from "lucide-react";
+import { z } from "zod";
+
+const chatTitleSchema = z.string().min(1, { message: "Title cannot be empty" });
+const emailSchema = z.string().email({ message: "Invalid email address" });
 
 const Navbar = () => {
-  const { title } = useSelector(
-    (state: RootState) => state.chat
-  );
+  const { title } = useSelector((state: RootState) => state.chat);
   const dispatch = useDispatch();
-  const [inputValue, setInputValue] = React.useState(title);
+  const [inputValue, setInputValue] = useState(title);
+  const [emailValue, setEmailValue] = useState(title);
+  const [inputError, setInputError] = useState<string | null>(null);
+  const [emailError, setEmailError] = useState<string | null>(null);
+
   const handleInputChange = (event: any) => {
     setInputValue(event.target.value);
   };
-const currentConversationId = usePathname()?.split("/").pop();
-  const {
-    refetch: refetchChatTitle,
-  } = useQuery({
+  
+  const handleEmailChange = (event: any) => {
+    setEmailValue(event.target.value);
+  };
+
+  const currentConversationId = usePathname()?.split("/").pop();
+  const { refetch: refetchChatTitle } = useQuery({
     queryKey: ["chat-title"],
     queryFn: async () => {
       const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
       const headers = await getHeaders();
-      axios.patch(`${process.env.NEXT_PUBLIC_CONVERSATION_BASE_URL}/api/v1/conversations/${currentConversationId}/rename/`, {
-        title: inputValue,
-      }, {headers:headers}).then((res) => {
-        if(res.data.status === "success") toast.success("Title updated successfully");
-        return res.data;
-      }).catch((err) => {
-        console.log(err);
-        return err.response.data;
-      });
+      axios
+        .patch(
+          `${process.env.NEXT_PUBLIC_CONVERSATION_BASE_URL}/api/v1/conversations/${currentConversationId}/rename/`,
+          { title: inputValue },
+          { headers: headers }
+        )
+        .then((res) => {
+          if (res.data.status === "success")
+            toast.success("Title updated successfully");
+          return res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+          return err.response.data;
+        });
     },
     enabled: false,
   });
+
   const handleSave = () => {
-    refetchChatTitle();
-    dispatch(setChat({ title: inputValue }));
+    try {
+      chatTitleSchema.parse(inputValue); 
+      setInputError(null);
+      refetchChatTitle();
+      dispatch(setChat({ title: inputValue }));
+    } catch (e) {
+      if (e instanceof z.ZodError) {
+        setInputError(e.errors[0].message); 
+      }
+    }
+  };
+
+  const handleEmailSave = () => {
+    try {
+      emailSchema.parse(emailValue); 
+      setEmailError(null);
+      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+      toast.success("Link copied to clipboard");
+      navigator.clipboard.writeText(baseUrl + "/chat/share" + currentConversationId);
+    } catch (e) {
+      if (e instanceof z.ZodError) {
+        setEmailError(e.errors[0].message); 
+      }
+    }
   };
 
   return (
@@ -67,31 +104,73 @@ const currentConversationId = usePathname()?.split("/").pop();
             Click here
           </Link>
         </div>
-        <div className="flex items-center w-full px-6 pb-2 gap-5 ">
-          <Image
-            src={"/images/msg-grey.svg"}
-            alt="logo"
-            width={20}
-            height={20}
-          />
+        <div className="flex w-full justify-between items-center">
+          <div className="flex items-center w-full px-6 pb-2 gap-5 ">
+            <Image
+              src={"/images/msg-grey.svg"}
+              alt="logo"
+              width={20}
+              height={20}
+            />
+            <Dialog>
+              <DialogTrigger>
+                <span className="text-muted text-xl">{title}</span>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[487px]" showX={false}>
+                <DialogHeader>
+                  <DialogTitle className="text-center">
+                    Edit chat name
+                  </DialogTitle>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="  ">
+                    <Input
+                      id="name"
+                      value={inputValue}
+                      onChange={handleInputChange}
+                      className="col-span-3"
+                    />
+                    {inputError && (
+                      <p className="text-red-500 text-sm">{inputError}</p>
+                    )}
+                  </div>
+                </div>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="button">Cancel</Button>
+                  </DialogClose>
+                  <DialogClose asChild>
+                    <Button type="button" onClick={handleSave}>
+                      Save
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
           <Dialog>
             <DialogTrigger>
-              <span className="text-muted text-xl">{title}</span>
+              <Button size="sm" className="gap-2 my-1">
+                <Share2Icon className="size-5" /> Share
+              </Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[487px]" showX={false}>
               <DialogHeader>
                 <DialogTitle className="text-center">
-                  Edit chat name
+                  Share chat with others
                 </DialogTitle>
               </DialogHeader>
               <div className="grid gap-4 py-4">
                 <div className="  ">
                   <Input
-                    id="name"
-                    value={inputValue}
-                    onChange={handleInputChange}
+                    id="email"
+                    placeholder="Email"
+                    onChange={handleEmailChange}
                     className="col-span-3"
                   />
+                  {emailError && (
+                    <p className="text-red-500 text-sm">{emailError}</p>
+                  )}
                 </div>
               </div>
               <DialogFooter>
@@ -99,8 +178,12 @@ const currentConversationId = usePathname()?.split("/").pop();
                   <Button type="button">Cancel</Button>
                 </DialogClose>
                 <DialogClose asChild>
-                  <Button type="button" onClick={handleSave}>
-                    Save
+                  <Button
+                    type="button"
+                    onClick={handleEmailSave}
+                    disabled={emailValue === ""}
+                  >
+                    share
                   </Button>
                 </DialogClose>
               </DialogFooter>

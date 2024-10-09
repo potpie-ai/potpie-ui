f4b3cf8171fe90b5c92b950188c0a1d14c01b20c
Vineet <vn.sh@outlook.com>
2024-10-09 18:02:25 +0530

    Update LayoutProviders.tsx

--- providers/LayoutProviders.tsx
+++ providers/LayoutProviders.tsx
@@ -11,9 +11,6 @@ import { PostHogProvider } from "posthog-js/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 if (typeof window !== "undefined") {
-  console.log("PostHog Key:", process.env.NEXT_PUBLIC_POSTHOG_KEY);
-  console.log("PostHog Host:", process.env.NEXT_PUBLIC_POSTHOG_HOST);
-
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     capture_pageview: true,

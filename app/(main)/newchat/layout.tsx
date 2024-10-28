44205ae974684cce0141e6c46ec5607affe36821
Yash Pede <yashpede@ssipmt.com>
2024-10-28 18:42:53 +0530

    bugfix: minor

--- app/(main)/newchat/layout.tsx
+++ app/(main)/newchat/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <Navbar showShare={false} showTitle={false} />
+      <Navbar showShare={false} />
       {children}
     </>
   );

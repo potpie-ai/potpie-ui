0d39b3c105aff55f0a829b75e6aa4cbd90859cfa
Yash Pede <yashpede@ssipmt.com>
2024-11-02 15:02:45 +0530

    feat: Feature flag implimentation

--- app/(main)/agents/template.tsx
+++ app/(main)/agents/template.tsx
@@ -4,25 +4,23 @@ import { useFeatureFlagEnabled } from "posthog-js/react";
 import React, { useEffect, useState } from "react";
 
 const NewAgents = ({ children }: { children: React.ReactNode }) => {
-  
-  // Feature flag implimentation temporary commented out
 
-  // const flagEnabled = useFeatureFlagEnabled("custom_agents");
-  // const router = useRouter();
-  // const [isFlagResolved, setIsFlagResolved] = useState(false);
-  // console.log(flagEnabled);
-  // useEffect(() => {
-  //   if (typeof flagEnabled !== "undefined") {
-  //     setIsFlagResolved(true);
-  //     if (!flagEnabled) {
-  //       router.back();
-  //     }
-  //   }
-  // }, [flagEnabled, router]);
+  const flagEnabled = useFeatureFlagEnabled("custom_agents");
+  const router = useRouter();
+  const [isFlagResolved, setIsFlagResolved] = useState(false);
+  console.log(flagEnabled);
+  useEffect(() => {
+    if (typeof flagEnabled !== "undefined") {
+      setIsFlagResolved(true);
+      if (!flagEnabled) {
+        router.back();
+      }
+    }
+  }, [flagEnabled, router]);
 
-  // if (!isFlagResolved) {
-  //   return router.back();
-  // }
+  if (!isFlagResolved) {
+    return router.back();
+  }
 
   return <>{children}</>;
 };

0a7a680255b53c0b2a99a6779ac42dc8d1a9da0e
Yash Pede <yashpede@ssipmt.com>
2024-11-02 13:59:56 +0530

    bugfix: minor

--- app/(main)/all-agents/page.tsx
+++ app/(main)/all-agents/page.tsx
@@ -105,7 +105,6 @@ const AllAgents = () => {
       setStatuses((prev) => ({ ...prev, [agentId]: "ERROR" }));
     },
   });
-  
 
   const stopAgent = useMutation({
     mutationFn: async (agentId: string) => {
@@ -129,7 +128,6 @@ const AllAgents = () => {
       setStatuses((prev) => ({ ...prev, [agentId]: "ERROR" }));
     },
   });
-  
 
   useEffect(() => {
     const handler = debounce((value) => {
@@ -194,65 +192,65 @@ const AllAgents = () => {
               const deploymentStatus = statuses[content.id];
               return (
                 <Card
-                key={index}
-                className="h-[200px] w-[485px] pt-2 border-border shadow-sm rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300 hover:border-[#FFB36E] hover:border-2 hover:shadow-md flex flex-col justify-between"
-              >
-                <CardHeader className="p-1 px-6 font-normal flex flex-row justify-between items-center">
-                  <CardTitle className="text-lg text-muted flex gap-2 items-center max-w-[380px]">
-                    <div className="truncate">{content.name}</div>
-                    <Bot className="flex-shrink-0" />
-                  </CardTitle>
-                </CardHeader>
-              
-                <CardContent className="px-6 pb-4 overflow-hidden text-base text-muted-foreground leading-tight max-w-[480px]">
-                  <p className="line-clamp-3">{content.description}</p>
-                </CardContent>
-              
-                <CardFooter className="flex items-center justify-end space-x-2">
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    className="hover:text-primary"
-                    onClick={() =>
-                      deploymentStatus !== "IN_PROGRESS" &&  deploymentStatus === "RUNNING"
-                        ? stopAgent.mutate(content.id)
-                        : deployAgent.mutate(content.id)
-                    }
-                  >
-                    {deploymentStatus === "ERRORED" ? (
-                      <AlertCircle className="size-5 text-red-600" />
-                    ) : !deploymentStatus ? (
-                      <Loader className="size-5 animate-spin" />
-                    ) : deploymentStatus === "RUNNING" ? (
-                      <Pause className="size-5" />
-                    ) : deploymentStatus === "IN_PROGRESS" ? (
-                      <Loader className="size-5 animate-spin" />
-                    ) : deploymentStatus === "STOPPED" ? (
-                      <Play className="size-5" />
-                    ) : (
-                      <AlertCircle className="size-5 text-red-600" />
-                    )}
-                  </Button>
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    className="hover:text-primary"
-                    onClick={() => deleteCustomAgentForm.mutate(content.id)}
-                  >
-                    <Trash className="size-5" />
-                  </Button>
-                  <Link href={`/agents?edit=${content.id}`}>
+                  key={index}
+                  className="h-[200px] w-[485px] pt-2 border-border shadow-sm rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300 hover:border-[#FFB36E] hover:border-2 hover:shadow-md flex flex-col justify-between"
+                >
+                  <CardHeader className="p-1 px-6 font-normal flex flex-row justify-between items-center">
+                    <CardTitle className="text-lg text-muted flex gap-2 items-center max-w-[380px]">
+                      <div className="truncate">{content.name}</div>
+                      <Bot className="flex-shrink-0" />
+                    </CardTitle>
+                  </CardHeader>
+
+                  <CardContent className="px-6 pb-4 overflow-hidden text-base text-muted-foreground leading-tight max-w-[480px]">
+                    <p className="line-clamp-3">{content.description}</p>
+                  </CardContent>
+
+                  <CardFooter className="flex items-center justify-end space-x-2">
                     <Button
                       variant="ghost"
                       size="icon"
                       className="hover:text-primary"
+                      onClick={() =>
+                        deploymentStatus === "RUNNING"
+                          ? stopAgent.mutate(content.id)
+                          : deploymentStatus !== "STOPPED" &&
+                            deployAgent.mutate(content.id)
+                      }
                     >
-                      <Edit className="w-5 h-5" />
+                      {deploymentStatus === "ERRORED" ? (
+                        <AlertCircle className="size-5 text-red-600" />
+                      ) : !deploymentStatus ? (
+                        <Loader className="size-5 animate-spin" />
+                      ) : deploymentStatus === "RUNNING" ? (
+                        <Pause className="size-5" />
+                      ) : deploymentStatus === "IN_PROGRESS" ? (
+                        <Loader className="size-5 animate-spin" />
+                      ) : deploymentStatus === "STOPPED" ? (
+                        <Play className="size-5" />
+                      ) : (
+                        <AlertCircle className="size-5 text-red-600" />
+                      )}
                     </Button>
-                  </Link>
-                </CardFooter>
-              </Card>
-              
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="hover:text-primary"
+                      onClick={() => deleteCustomAgentForm.mutate(content.id)}
+                    >
+                      <Trash className="size-5" />
+                    </Button>
+                    <Link href={`/agents?edit=${content.id}`}>
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="hover:text-primary"
+                      >
+                        <Edit className="w-5 h-5" />
+                      </Button>
+                    </Link>
+                  </CardFooter>
+                </Card>
               );
             }
           )

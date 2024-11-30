ea363207c9378d0a4f151b748e0768e7773f53d9
Yash Pede <yashpede@ssipmt.com>
2024-11-30 16:32:54 +0530

    bugfix: redirection on signup too

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -22,13 +22,26 @@ import {
   sendBlue,
   setting,
 } from "@/public";
-import { LucideCheck, LucideGithub } from "lucide-react";
+import { LucideGithub } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useCallback } from "react";
 import Link from "next/link";
 import posthog from "posthog-js";
+import { useSearchParams } from "next/navigation";
 
 export default function Signin() {
+  const searchParams = useSearchParams();
+
+  const createQueryString = useCallback(
+    (name?: string, value?: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      if (name && value) params.append(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
   const formSchema = z.object({
     email: z.string().email(),
     password: z.string().min(6, { message: "Password is required" }),
@@ -42,8 +55,8 @@ export default function Signin() {
   });
 
   const provider = new GithubAuthProvider();
-  provider.addScope('read:org');
-  provider.addScope('user:email');
+  provider.addScope("read:org");
+  provider.addScope("user:email");
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     signInWithEmailAndPassword(auth, data.email, data.password)
@@ -52,7 +65,7 @@ export default function Signin() {
         const user = userCredential.user;
         const headers = await getHeaders();
         const userSignup = axios
-          .post(`${baseUrl}/api/v1/signup`, user,{headers:headers})
+          .post(`${baseUrl}/api/v1/signup`, user, { headers: headers })
           .then((res) => res.data)
           .catch((e) => {
             toast.error("Signup call unsuccessful");
@@ -79,7 +92,8 @@ export default function Signin() {
             .post(`${baseUrl}/api/v1/signup`, {
               uid: result.user.uid,
               email: result.user.email,
-              displayName: result.user.displayName || result.user.email?.split("@")[0],
+              displayName:
+                result.user.displayName || result.user.email?.split("@")[0],
               emailVerified: result.user.emailVerified,
               createdAt: result.user.metadata?.creationTime
                 ? new Date(result.user.metadata.creationTime).toISOString()
@@ -96,11 +110,11 @@ export default function Signin() {
             .catch((e: any) => {
               toast.error("Signup call unsuccessful");
             });
-            posthog.identify(
-              result.user.uid,
-              { email: result.user.email, name: result.user?.displayName || "" }
-            );
-          
+          posthog.identify(result.user.uid, {
+            email: result.user.email,
+            name: result.user?.displayName || "",
+          });
+
           toast.success("Logged in successfully");
         }
       })
@@ -114,49 +128,49 @@ export default function Signin() {
   };
   return (
     <section className="flex items-center justify-between w-full h-screen relative">
-    <div className="w-full bg-[#515983] h-full flex flex-col items-center justify-center gap-28">
-      <div className="h-3/6 w-3/5 bg-white rounded-3xl relative">
-        <div className="flex justify-between flex-col w-full h-full">
-          <div className="mt-auto py-6 px-4 flex flex-col gap-5 h-full">
-            <div className="flex items-start justify-start gap-2">
-              <div className="w-10 h-10 bg-[#FFF1E0] rounded-full grid place-items-center text-primary">
-                AK
+      <div className="w-full bg-[#515983] h-full flex flex-col items-center justify-center gap-28">
+        <div className="h-3/6 w-3/5 bg-white rounded-3xl relative">
+          <div className="flex justify-between flex-col w-full h-full">
+            <div className="mt-auto py-6 px-4 flex flex-col gap-5 h-full">
+              <div className="flex items-start justify-start gap-2">
+                <div className="w-10 h-10 bg-[#FFF1E0] rounded-full grid place-items-center text-primary">
+                  AK
+                </div>
+                <div className="bg-[#FFF1E0] h-20 w-[70%] rounded-lg"></div>
               </div>
-              <div className="bg-[#FFF1E0] h-20 w-[70%] rounded-lg"></div>
+              <div className="flex items-start justify-end gap-2">
+                <div className="bg-[#E0F3FF] h-20 w-[70%] rounded-lg"></div>
+                <div className="w-10 h-10 bg-[#FFF1E0] rounded-full grid place-items-center text-primary">
+                  <Image src={logo60} alt="logo60" className="rounded-full" />
+                </div>
+              </div>
+              <div className="flex items-start justify-start gap-2">
+                <div className="w-10 h-10 bg-[#FFF1E0] rounded-full grid place-items-center text-primary">
+                  AK
+                </div>
+                <div className="bg-[#FFF1E0] h-10 w-[70%] rounded-lg"></div>
+              </div>
             </div>
-            <div className="flex items-start justify-end gap-2">
-              <div className="bg-[#E0F3FF] h-20 w-[70%] rounded-lg"></div>
-              <div className="w-10 h-10 bg-[#FFF1E0] rounded-full grid place-items-center text-primary">
-                <Image src={logo60} alt="logo60" className="rounded-full" />
-              </div>
-            </div>
-            <div className="flex items-start justify-start gap-2">
-              <div className="w-10 h-10 bg-[#FFF1E0] rounded-full grid place-items-center text-primary">
-                AK
-              </div>
-              <div className="bg-[#FFF1E0] h-10 w-[70%] rounded-lg"></div>
+            <div className="mb-6 w-5/6 mx-auto h-10 flex justify-end border-2 rounded-sm border-[#3E99DB]">
+              <Image src={sendBlue} alt="sendBlue" className="ml-auto mr-2" />
             </div>
           </div>
-          <div className="mb-6 w-5/6 mx-auto h-10 flex justify-end border-2 rounded-sm border-[#3E99DB]">
-            <Image src={sendBlue} alt="sendBlue" className="ml-auto mr-2" />
-          </div>
+          <Image
+            src={arrowcon}
+            className="absolute -bottom-[6rem] right-0 "
+            alt="arrowcon"
+          />
+          <Image src={cross} className="absolute top-0 -right-11" alt="cross" />
         </div>
-        <Image
-          src={arrowcon}
-          className="absolute -bottom-[6rem] right-0 "
-          alt="arrowcon"
-        />
-        <Image src={cross} className="absolute top-0 -right-11" alt="cross" />
+        <div className="text-xl text-center text-white font-bold mb-10">
+          Build AI agents specialised on your <br /> codebase in a minute
+        </div>
       </div>
-      <div className="text-xl text-center text-white font-bold mb-10">
-        Build AI agents specialised on your <br /> codebase in a minute
-      </div>
-    </div>
-    <div className="w-full h-full flex items-center justify-center flex-col gap-14">
-      <Image src={logoWithText} alt="logo" />
-      <div className="flex items-center justify-center flex-col text-border">
-        {/* <h3 className="text-2xl font-bold text-black">Get Started!</h3> */}
-        {/* <div className="flex items-start justify-start flex-col mt-10 gap-4">
+      <div className="w-full h-full flex items-center justify-center flex-col gap-14">
+        <Image src={logoWithText} alt="logo" />
+        <div className="flex items-center justify-center flex-col text-border">
+          {/* <h3 className="text-2xl font-bold text-black">Get Started!</h3> */}
+          {/* <div className="flex items-start justify-start flex-col mt-10 gap-4">
           <p className="flex items-center justify-center text-start gap-4">
             <LucideCheck
               size={20}
@@ -173,21 +187,24 @@ export default function Signin() {
             dashboard
           </p>
         </div> */}
-        <Button onClick={() => onGithub()} className="mt-14 gap-2 w-80">
-          <LucideGithub className=" rounded-full border border-white p-1" />
-          Signin with GitHub
-        </Button>
-        <div className="mt-4 text-center text-sm">
+          <Button onClick={() => onGithub()} className="mt-14 gap-2 w-80">
+            <LucideGithub className=" rounded-full border border-white p-1" />
+            Signin with GitHub
+          </Button>
+          <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
-            <Link href="/sign-up" className="underline">
+            <Link
+              href={"/sign-up" + "?" + createQueryString()}
+              className="underline"
+            >
               Sign up
             </Link>
           </div>
+        </div>
       </div>
-    </div>
-    <Image src={chat} className="absolute top-0" alt="chat" />
-    <Image src={cloud} className="absolute bottom-2" alt="cloud" />
-    <Image src={setting} className="absolute top-0 right-0" alt="setting" />
-  </section>
+      <Image src={chat} className="absolute top-0" alt="chat" />
+      <Image src={cloud} className="absolute bottom-2" alt="cloud" />
+      <Image src={setting} className="absolute top-0 right-0" alt="setting" />
+    </section>
   );
 }
